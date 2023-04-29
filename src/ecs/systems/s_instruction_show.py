adc1b74ec95ee34282708d9bc2debe0ebddbbb69
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-04-29 18:37:31 -0500

    message updated

--- src/ecs/systems/s_instruction_show.py
+++ src/ecs/systems/s_instruction_show.py
@@ -29,11 +29,18 @@ async def system_instruction_show(screen: pygame.Surface):
     textpos4.centerx = screen.get_rect().centerx
     textpos4.centery = screen.get_rect().centery + 150
 
+    # Write some other text
+    text5 = font.render("To pause the game you can press P", True, (255, 255, 255))
+    textpos5 = text5.get_rect()
+    textpos5.centerx = screen.get_rect().centerx
+    textpos5.centery = screen.get_rect().centery + 70
+
     # Display the text
     screen.blit(text, textpos)
     screen.blit(text2, textpos2)
     screen.blit(text3, textpos3)
     screen.blit(text4, textpos4)
+    screen.blit(text5, textpos5)
     pygame.display.update()
 
     while True:
